cc1799ac2cfa95f9a518067e16bbb49eecc24af2
Pedro Marques <roque@pedros-macbook-pro.home>
2019-03-22 08:49:20 +0100

    Add installer.

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(name='keras-opt',
+                 author='Pedro Marques',
+                 author_email='pedro.r.marques@gmail.com',
+                 description='Scipy keras optimizer',
+                 url='https://github.com/pedro-r-marques/keras-opt',
+                 packages=setuptools.find_packages(),
+                 install_requires = [
+                     'numpy', 'tesorflow', 'scipy', 'tqdm'
+                 ],
+                 python_requires='>=3.6',
+                 version='0.0.1')
